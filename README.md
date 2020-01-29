ee575ae1f8af6e93f157c1dcb4fe907686c98e9c
Kevin K H Zheng <172853425@qq.com>
2020-01-29 20:00:32 +0800

    其余并发容器讲解；安全共享对象策略 - 总结

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # learn_concurrency
+
+
+
+![image-20200129121441134](https://tva1.sinaimg.cn/large/006tNbRwgy1gbdb5smnbbj31hc0tsncv.jpg)
--- src/test/java/com/learn/concurrency/collections/concurrent/SafeCollectionsExample.java
+++ src/test/java/com/learn/concurrency/collections/concurrent/SafeCollectionsExample.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.*;
 
 @Slf4j
@@ -37,5 +39,102 @@ public class SafeCollectionsExample {
         countDownLatch.await();
         executorService.shutdown();
         log.info("size : {}", list.size());
+    }
+
+    @ThreadSafe
+    @Test
+    public void copyOnWriteArraySetExample() throws Exception{
+        //请求总数
+        int requestCount = 5000;
+        CountDownLatch countDownLatch = new CountDownLatch(requestCount);
+        Set<Integer> list = new CopyOnWriteArraySet<>();
+        //并发请求数
+        int concurrentCount = 50;
+        Semaphore semaphore = new Semaphore(concurrentCount);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < requestCount; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    list.add(finalI);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size : {}", list.size());
+    }
+
+    /**
+     * HashMap -> ConcurrentHashMap
+     *
+     * @throws Exception
+     */
+    @ThreadSafe
+    @Test
+    public void concurrentHashMapExample() throws Exception{
+        //请求总数
+        int requestCount = 5000;
+        CountDownLatch countDownLatch = new CountDownLatch(requestCount);
+        Map<Integer,Integer> map = new ConcurrentHashMap<>();
+        //并发请求数
+        int concurrentCount = 50;
+        Semaphore semaphore = new Semaphore(concurrentCount);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < requestCount; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    map.put(finalI,finalI);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size : {}", map.size());
+    }
+
+    /**
+     * TreeMap -> ConcurrentSkipListMap
+     *
+     * @throws Exception
+     */
+    @ThreadSafe
+    @Test
+    public void concurrentSkipListMapExample() throws Exception{
+        //请求总数
+        int requestCount = 5000;
+        CountDownLatch countDownLatch = new CountDownLatch(requestCount);
+        Map<Integer,Integer> map = new ConcurrentSkipListMap<>();
+        //并发请求数
+        int concurrentCount = 50;
+        Semaphore semaphore = new Semaphore(concurrentCount);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < requestCount; i++) {
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    map.put(finalI,finalI);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size : {}", map.size());
     }
 }
