1a534d0a254c9817c3916db7b7242e780281115f
Kevin K H Zheng <172853425@qq.com>
2020-01-30 17:59:29 +0800

    AQS - ReentrantLock

--- src/test/java/com/learn/concurrency/lock/CountExample.java
+++ src/test/java/com/learn/concurrency/lock/CountExample.java
@@ -1,6 +1,7 @@
 package com.learn.concurrency.lock;
 
 import com.learn.concurrency.annotation.NotThreadSafe;
+import com.learn.concurrency.annotation.ThreadSafe;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -9,6 +10,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 使用锁进行计数
@@ -23,7 +25,7 @@ public class CountExample {
      *
      * @throws Exception 忽略异常
      */
-    @NotThreadSafe
+    @ThreadSafe
     @Test
     public void count() throws Exception {
         long startTime = new Date().getTime();
@@ -41,6 +43,37 @@ public class CountExample {
                     synchronized (this){
                         COUNT++;
                     }
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.error("count = {}", COUNT);
+        log.info("total time = {} ms", new Date().getTime() - startTime);
+    }
+
+    @Test
+    public void reentrantLock() throws Exception {
+        long startTime = new Date().getTime();
+        //请求总数
+        int requestCount = 5000;
+        CountDownLatch countDownLatch = new CountDownLatch(requestCount);
+        //并发请求数
+        int concurrentCount = 50;
+        Semaphore semaphore = new Semaphore(concurrentCount);
+        ReentrantLock lock = new ReentrantLock();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < requestCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    lock.lock();
+                    COUNT++;
+                    lock.unlock();
                     semaphore.release();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
